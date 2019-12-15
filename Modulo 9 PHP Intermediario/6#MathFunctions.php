fb73da725aef48dd3c398ba7a8e31b5f96ae859b
Alvez <joaor4e@gmail.com>
2019-12-15 18:18:35 -0300

    Exercicios com funções matematicas no php

---
+++ Modulo 9 PHP Intermediario/6#MathFunctions.php
@@ -0,0 +1,28 @@
+<?php
+
+    //abs retorna o valor absoluto
+    echo abs(-10);
+
+    echo "<hr>";
+
+    //round arrendonda um numero baseado no numero
+    echo round(1.6);
+
+    echo "<hr>";
+
+    //ceil arrendonda pra cima
+    echo ceil(2.1);
+
+    echo "<hr>";
+
+    //floor o contrario do ceil
+    echo floor(27.99);
+
+    echo "<hr>";
+
+    //rand retorna um inteiro aleatorio entre um numero e outro
+    echo rand(0,100);
+
+    echo "<hr>";
+
+?>
