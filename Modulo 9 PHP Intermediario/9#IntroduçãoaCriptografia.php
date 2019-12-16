e0b64345dc00a58e2f1ad4a3af086da68c1b8186
Alvez <joaor4e@gmail.com>
2019-12-16 20:50:36 -0300

    Introdução ao basico da decodificação

---
+++ Modulo 9 PHP Intermediario/9#IntroduçãoaCriptografia.php
@@ -0,0 +1,14 @@
+<?php
+    //md5 tipo de codificação irreversivel base64 tipo de codificação reversivel
+    $nome = "Bonieky";
+    $nome2 = base64_encode($nome);
+
+    echo "Nome Original: ".$nome."<br>";
+    echo "Nome Criptografado: ".$nome2."<br>";
+
+    echo "<hr>";
+
+    echo "Texto Original: ".$nome2."<br>";
+    echo "texto Decodificado: ".base64_decode($nome2)."<br>";
+    
+?>
